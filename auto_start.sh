2ed745afd6a3d49f8291c6e102477328e6aa1638
Hugo-C <Hugo-C@users.noreply.github.com>
2023-06-18 22:14:12 +0200

    Start radio on boot

--- auto_start.sh
+++ auto_start.sh
@@ -1,2 +1,20 @@
-bluetoothctl connect $RADIO_PI_MAC_ADDRESS
+#!/usr/bin/env bash
+
+echo "Starting radio-pi auto_start.sh"
+pulseaudio --start
+echo "connecting ..."
+while true
+do
+  bluetoothctl connect $RADIO_PI_MAC_ADDRESS
+  if [ $? -eq 0 ]; then
+    break;
+  else
+    echo "retrying bluetooth connection ..."
+    sleep 10
+  fi
+done
+echo "connected !"
+sleep 2
+which python
 python radio_pi.py
+echo "done !"
--- radio_pi.py
+++ radio_pi.py
@@ -1,14 +1,20 @@
 import vlc
 
+from time import sleep
 from settings import URL, VOLUME
 
 if __name__ == '__main__':
-    instance = vlc.Instance()
+    instance = vlc.Instance("--h264-fps=15")  # fix lag
     player = instance.media_player_new()
     media = instance.media_new(URL)
     player.set_media(media)
 
     player.audio_set_volume(VOLUME)
     player.play()
-    input("press enter to quit")
-    player.stop()
+
+    playing = True  # Assume play is started or will start soon
+    print(f"Playing {URL}")
+
+    while playing:
+        sleep(10)
+        playing = player.is_playing()
